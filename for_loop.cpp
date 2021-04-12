07e8ccac9708650aa4801db9d9248c4eed1c405d
Armend Veseli <armend.veseli@yahoo.com>
2021-04-12 05:02:55 -0400

    Create for_loop.cpp

---
+++ for_loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+  
+  for(int count_down = 10; count_down > 0; count_down--)
+  {
+    std::cout << count_down << "\n";
+  }
+  
+  std::cout << "liftoff!";
+  
+  return 0;
+}
