c0e2a66f244cdd7790e14a7750732da301ceeebc
Armend Veseli <1509793+Vodkacannon@users.noreply.github.com>
2024-02-10 03:46:19 -0500

    Update switch.cpp

--- switch.cpp
+++ switch.cpp
@@ -1,8 +1,9 @@
-switch (my_char) {
-    case '0':
-        return 0;
-    case '1':
-        return 1;
-    default:
-        return -1;
+switch (my_char) 
+{
+case '0':
+    return 0;
+case '1':
+    return 1;
+default:
+    return -1;
 }
